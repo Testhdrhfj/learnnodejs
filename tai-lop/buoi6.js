b57ebb86c69aba173dfb67da3df111c053552843
abc <abc@example.com>
2023-09-28 19:51:52 +0700

    update inarr

--- tai-lop/buoi6.js
+++ tai-lop/buoi6.js
@@ -10,5 +10,16 @@ function maxArr(arr) {
     return max
 }
 
-var arr = [0, 656, 7, 8, 1, 123, 6]
-console.log(maxArr(arr))
+// var arr = [0, 656, 7, 8, 1, 123, 6]
+// console.log(maxArr(arr))
+
+
+var arr1 = ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
+function inArr(arr) {
+    var a = ""
+    for (var i = 0; i < arr.length; i += 1) {
+        a += arr[i]
+    }
+    return a
+}
+console.log(inArr(arr1))
