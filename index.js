840cc1f2ca45a6454cab070a844400cd7455bbe2
abc <abc@example.com>
2023-09-28 19:13:24 +0700

    update tai-lop

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-function maxArr(arr) {
-    var max = 0
-    for (var i = 0; i < arr.length; i += 1) {
-        if (arr[i] > max) {
-            max = arr[i]
-        }
-    }
-    return max
-}
-
-var arr = [1, 65, 7, 8, 1, 123,6]
-console.log(maxArr(arr))
+console.log("hello world")
---
+++ tai-lop/buoi6.js
@@ -0,0 +1,12 @@
+function maxArr(arr) {
+    var max = 0
+    for (var i = 0; i < arr.length; i += 1) {
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    return max
+}
+
+var arr = [0, 656, 7, 8, 1, 123,6]
+console.log(maxArr(arr))
