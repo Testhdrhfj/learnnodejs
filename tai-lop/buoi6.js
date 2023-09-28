8dd953f530831a75bfa53eb2d7376dde38fb2411
abc <abc@example.com>
2023-09-28 19:19:14 +0700

    update 2

--- tai-lop/buoi6.js
+++ tai-lop/buoi6.js
@@ -3,10 +3,12 @@ function maxArr(arr) {
     for (var i = 0; i < arr.length; i += 1) {
         if (arr[i] > max) {
             max = arr[i]
+        } else {
+
         }
     }
     return max
 }
 
-var arr = [0, 656, 7, 8, 1, 123,6]
+var arr = [0, 656, 7, 8, 1, 123, 6]
 console.log(maxArr(arr))
